f4738d499b088a8dca99bdfaba1c487780614819
Josheir <MatrixMan961@gmail.com>
2022-03-07 15:13:02 -0500

    added equals sign

--- main.go
+++ main.go
@@ -808,7 +808,7 @@ func createCartTemplate(w http.ResponseWriter, r *http.Request) {
 		for i = 0; i < (length); i++ {
 
 			allProductIds = append(allProductIds, []string{r.Form["id"][i]}...)
-			allPurchaseAmounts = append(allPurchaseAmounts, []string{r.Form["quant"][i]}...)
+			allPurchaseAmounts = append(allPurchaseAmounts, []string{r.Form["amtTryingToPurchase"][i]}...)
 
 		}
 
