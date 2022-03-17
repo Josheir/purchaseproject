727d89c45cf627d02c707673a088deb6de32a909
Josheir <MatrixMan961@gmail.com>
2022-03-17 15:25:25 -0400

    wrote function

--- main1.js
+++ main1.js
@@ -3,3 +3,37 @@ var currency = require("./node_modules/currency.js");
 var a = currency(1);
 alert(a);
 
+
+
+//$xxx.xx
+//var moneyAmt = GetTotalWithTaxFromAmtProduct(costID, buyThisManyID);
+//$xxx.xx                               //$xxx.xx
+//var summedMoneyAmt = GetSummedGrandTotal(moneyAmt);
+
+
+//updates cart
+function GetTotalWithTaxFromAmtProduct(costID, buyThisManyID, totalCostID, GrandTotalStringID)
+{
+
+    
+    var costForEachProducts = document.getElementById(costID).value;
+    var buyThisAmt = document.getElementById(buyThisManyID).value;
+    
+    //$xxx.xx
+    var total = ((currency(costForEachProducts).multiply(currency(buyThisAmt))).format());
+    //document.getElementById(GrandTotalStringID).value;
+    //$xxx.xx
+    document.getElementById(totalCostID).value = total;
+
+
+//$xxx.xx    
+return total ;
+
+}
+                            //$xxx.xx
+function GetSummedGrandTotal(moneyAmt)
+{
+
+    //$xxx.xx
+    return ;
+}
